3562cfafaf0efe83e25ea720b8736d3626ee6146
Joseph C. Sible <josephcsible@users.noreply.github.com>
2016-11-16 17:06:47 -0500

    Bump version to 1.0.1

--- src/main/java/josephcsible/webshooter/WebShooter.java
+++ src/main/java/josephcsible/webshooter/WebShooter.java
@@ -46,7 +46,7 @@ public class WebShooter
 {
 	// XXX duplication with mcmod.info and build.gradle
 	public static final String MODID = "webshooter";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.0.1";
 
 	public static Configuration config;
 	public static SimpleNetworkWrapper netWrapper;
